b6a4e9ed84eb36ed07412a54a8ad5424b59eec09
Konrad Dąbrowski <konrad.dabrowski.90@gmail.com>
2021-05-21 23:29:36 +0200

    test commit

--- ExceptionResponse.cs
+++ ExceptionResponse.cs
@@ -4,7 +4,7 @@ namespace Arcaim.Exception
 {
     public class ExceptionResponse
     {
-        public object Response { get; }
+        public object Response { get; }//
         public HttpStatusCode StatusCode { get; }
 
         public ExceptionResponse(object response, HttpStatusCode statusCode)
