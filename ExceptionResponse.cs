68fa18318c7c888823f111f4184f187a1b6e4aa6
Konrad Dąbrowski <konrad.dabrowski.90@gmail.com>
2021-07-15 22:58:15 +0200

    Update

--- ExceptionResponse.cs
+++ ExceptionResponse.cs
@@ -4,10 +4,11 @@ namespace Arcaim.Exception
 {
     public class ExceptionResponse
     {
-        public object Response { get; }//
+        public object Response { get; }
         public HttpStatusCode StatusCode { get; }
+        public string Source { get; }
 
-        public ExceptionResponse(object response, HttpStatusCode statusCode)
-            => (Response, StatusCode) = (response, statusCode);
+        public ExceptionResponse(object response, HttpStatusCode statusCode, string source)
+            => (Response, StatusCode, Source) = (response, statusCode, source);
     }
 }
